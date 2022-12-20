1f27643dfb8eb08d4c0b4baabc595f0a3b8feb20
ibacon66 <315675274@qq.com>
2022-12-20 13:21:48 +0800

    统一结果返回

---
+++ common/common-utils/src/main/java/com/ibacon/commonutils/ResultCode.java
@@ -0,0 +1,13 @@
+package com.ibacon.commonutils;
+
+/**
+ * @author ibacon66
+ * @Description 状态码
+ * @createTime 2022年12月20日 12:23:00
+ */
+public interface ResultCode {
+
+    Integer SUCCESS = 20000;
+
+    Integer ERROR = 20001;
+}
--- service/service-edu/src/main/java/com/ibacon/eduservice/controller/EduTeacherController.java
+++ service/service-edu/src/main/java/com/ibacon/eduservice/controller/EduTeacherController.java
@@ -1,8 +1,10 @@
 package com.ibacon.eduservice.controller;
 
 
+import com.ibacon.commonutils.R;
 import com.ibacon.eduservice.entity.EduTeacher;
 import com.ibacon.eduservice.service.EduTeacherService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,13 +23,17 @@ import java.util.List;
 public class EduTeacherController {
     @Autowired
     private EduTeacherService teacherService;
+    @ApiOperation(value = "所有讲师列表")
     @GetMapping("findAll")
-    public List<EduTeacher> findAllTeacher(){
-        return teacherService.list(null);
+    public R list(){
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items",list);
     }
+    @ApiOperation(value = "根据ID删除讲师")
     @DeleteMapping("{id}")
-    public boolean removeById(@PathVariable String id){
-        return teacherService.removeById(id);
+    public R removeById(@PathVariable String id){
+        teacherService.removeById(id);
+        return R.ok();
     }
 
 
